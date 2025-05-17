c8d7341a1c98bd3f65768af217c0c2936f3302c3
SamuelDevelop <ssoaresdossantos088@gmail.com>
2025-05-17 12:05:45 -0300

    commit inicial teste - 17/05/25

---
+++ NetBeansProjects/LaboratorioLP/src/main/java/com/cefetmg/lista01/P1/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cefetmg.lista01.P1;
+
+/**
+ *
+ * @author smuu
+ */
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+ 
+}
---
+++ NetBeansProjects/LaboratorioLP/src/main/java/com/cefetmg/lista01/P6/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cefetmg.lista01.P6;
+
+/**
+ *
+ * @author smuu
+ */
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		
+		int diferenca = (a*b - c*d);
+		System.out.printf("DIFERENCA = %d\n", diferenca);
+	}
+}
