6334c3d698f74a3e80383e95d4bbb30824196bde
SamuelDevelop <ssoaresdossantos088@gmail.com>
2025-05-17 11:36:40 -0300

    commit inicial teste - 17/05/25

--- NetBeansProjects/LaboratorioLP/src/main/java/com/mycompany/laboratoriolp/LaboratorioLP.java
+++ NetBeansProjects/LaboratorioLP/src/main/java/com/mycompany/laboratoriolp/LaboratorioLP.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class LaboratorioLP {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
+        System.out.println("tESTE!");
     }
  
 }
