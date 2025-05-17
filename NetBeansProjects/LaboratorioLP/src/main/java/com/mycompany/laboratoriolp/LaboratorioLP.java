7c333ff60bc8b57248161f5fbbe4121a674cf4ca
SamuelDevelop <ssoaresdossantos088@gmail.com>
2025-05-17 11:29:52 -0300

    commit inicial teste - 17/05/25

---
+++ NetBeansProjects/LaboratorioLP/src/main/java/com/mycompany/laboratoriolp/LaboratorioLP.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratoriolp;
+
+/**
+ *
+ * @author smuu
+ */
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+ 
+}
