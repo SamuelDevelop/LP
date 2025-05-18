201cf7b2a87c59cbbab93d33c84f406e60b15842
SamuelDevelop <ssoaresdossantos088@gmail.com>
2025-05-18 19:02:28 -0300

    commit inicial - 18/05/25

--- NetBeansProjects/LaboratorioLP/src/main/java/com/mycompany/laboratoriolp/LaboratorioLP.java
+++ NetBeansProjects/LaboratorioLP/src/main/java/com/mycompany/laboratoriolp/LaboratorioLP.java
@@ -8,12 +8,9 @@ package com.mycompany.laboratoriolp;
  *
  * @author smuu
  */
-import java.io.IOException;
-
 public class LaboratorioLP {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("tESTE!");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
- 
 }
