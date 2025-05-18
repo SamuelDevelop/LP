d4896e062ecf08b2a3bd3beb75cb9cd5a3895e8d
SamuelDevelop <ssoaresdossantos088@gmail.com>
2025-05-18 19:21:32 -0300

    commit 2

---
+++ NetBeansProjects/LaboratorioLP/src/main/java/br/cefetmg/Lista01/P1006/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.cefetmg.Lista01.P1006;
+
+/**
+ *
+ * @author smuu
+ */
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		
+		double media = ((a*2) + (b*3) + (c*5)) / 10;
+		System.out.printf("MEDIA = %.1f%n", media);
+	}
+}
---
+++ NetBeansProjects/LaboratorioLP/src/main/java/br/cefetmg/Lista01P1035/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.cefetmg.Lista01P1035;
+
+/**
+ *
+ * @author smuu
+ */
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    int c = sc.nextInt();
+	    int d = sc.nextInt();
+	    if(b>c && d>a && c+d > a+b && c>= 0 && d>= 0 && a%2 == 0){
+	        System.out.printf("Valores aceitos\n");
+	    }
+	    else{
+	        System.out.printf("Valores nao aceitos\n");
+	    }
+
+
+	}
+}
