1a32b74e03ced10f53c173a6ebc3f7837db77778
SamuelDevelop <ssoaresdossantos088@gmail.com>
2025-05-18 22:13:03 -0300

    commit 3 - Lista 02

--- NetBeansProjects/LaboratorioLP/src/main/java/br/cefetmg/Lista01/P1052/Main.java
+++ NetBeansProjects/LaboratorioLP/src/main/java/br/cefetmg/Lista01/P1052/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.cefetmg.Lista01P1052;
+package br.cefetmg.Lista01.P1052;
 
 /**
  *
---
+++ NetBeansProjects/LaboratorioLP/src/main/java/br/cefetmg/Lista02/P1245/Main.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.cefetmg.Lista02.P1245;
+
+/**
+ *
+ * @author smuu
+ */
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int[] numerosEsquerda = new int[61];
+            int[] numerosDireita = new int[61];
+
+            for (int i = 0; i < n; i++) {
+                int numero = sc.nextInt();
+                char ladoPe = sc.next().charAt(0);
+                if (ladoPe == 'E') {
+                    numerosEsquerda[numero]++;
+                } else if (ladoPe == 'D') {
+                    numerosDireita[numero]++;
+                }
+            }
+
+            int totalPares = 0;
+            for (int i = 30; i <= 60; i++) {
+                if (numerosEsquerda[i] < numerosDireita[i]) {
+                    totalPares += numerosEsquerda[i];
+                } else {
+                    totalPares += numerosDireita[i];
+                }
+            }
+
+            System.out.println(totalPares);
+        }
+    }
+}
